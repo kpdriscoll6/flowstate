1a8e35e379bd715ccf3aa4422797cecb635f0e64
Kevin Driscoll <kpdriscoll6@gmail.com>
2020-07-26 19:09:20 -0600

    finished parseID script

--- parseReachID.py
+++ parseReachID.py
@@ -4,77 +4,84 @@ import os
 import xarray as xr
 import shapefile
 import wget
+from tqdm import tqdm
 
 
 def parseID():
-    #This function saves a CSV of the GPS coords of each Reach ID
-    directory = os.getcwd()+'\\NWM_parameters\\'
-    for root, dirs, files in os.walk(directory): 
-        for file in files:
-            if file.startswith('Route'):
-                filePath = os.path.join(root, file)
-                nc = netCDF4.Dataset(filePath, mode='r')
-                #print(nc.variables.keys())
-                ds = xr.open_dataset(filePath)
-                df = ds.to_dataframe()
-                df.reset_index(inplace = True)
-                ##FOR SOME REASON THE INDEX RESET SHIFTS THE FEATURE ID IN THE LINK COLUMN (MAYBE XARRAY ISSUE)
-                ##going to rename columns to account for it
-                df = df.filter(['link','lat','lon'], axis=1)
-                df = df.rename(columns={'link':'feature_id'})
-
-                ##losing information with Xarray i think...
-                #fh = filehandle
-                reachData = Dataset(filePath, mode='r')
-                #print(fh.variables)
-                #FROM VARIABLE IS INTERESTING...
-                lat  = reachData.variables['lat'][:]
-                lon = reachData.variables['lon'][:]
-                altitude = reachData.variables['alt'][:]
-                slope = reachData.variables['slope'][:]
-                print(len(vals))
-                #for val in vals:
-                    #print(val)
-                #print(fh.variables['link'][:])
-                #df.to_csv('reachIDs.csv')
+    #This function saves a CSV of the GPS coords of each Reach ID and appends the river name from the flow_table.txt file
+    flowTable = pd.read_csv(os.getcwd()+'\\NWM_parameters\\COMID_Names.csv', index_col=0)
+    filePath = os.getcwd()+ '\\NWM_parameters\\RouteLink_NHDPLUS.nc'
+    nc = netCDF4.Dataset(filePath, mode='r')
+    ds = xr.open_dataset(filePath)
+    df = ds.to_dataframe()
+    #print(df.head())
+    df.reset_index(inplace = True)
+    #print(df.head())
+    ##FOR SOME REASON THE INDEX RESET SHIFTS THE FEATURE ID IN THE LINK COLUMN (MAYBE XARRAY ISSUE)
+    ##going to rename columns to account for it
+    df = df.filter(['link','NHDWaterbodyComID','lat','lon'], axis=1)
+    df = df.rename(columns={'link':'feature_id'})
+    #add a column for name
+    df['name'] = ''
+    #COMIDs = df['NHDWaterbodyComID']
+    COMIDs = df['feature_id']
+    for COMID in tqdm(COMIDs):
+        #check if the COMID is in the flowTable & add that to the datafram if it exists
+        if COMID in flowTable.COMID.values:
+            name = flowTable.loc[flowTable['COMID']==COMID].values.tolist()[0][1]
+            df.loc[df['feature_id']==COMID, ['name']] = name
+            #SYNTAX FOR REPLACEMENT
+            #df.loc[df[<some_column_name>] == <condition>, [<another_column_name>]] = <value_to_add>
+            #print(df.loc[df['feature_id']==COMID])
+    df.to_csv('reachIDs.csv')
+                
     return
-#parseID()
+
+#print(flowTable.head())
+#name = flowTable.loc[flowTable['COMID']==721640]['GNIS_NAME'].values[0]
+#sprint(name)
+parseID()
 
 
 
-directory = os.getcwd()
-for root, dirs, files in os.walk(directory): 
-    for file in files:
-        if file.startswith('stateNames'):
-            filePath = os.path.join(root, file)
-            states = pd.read_csv(filePath)
-states = states['State'].values.tolist()
-for state in states:
-    try:
-        url1 = 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHD/State/HighResolution/Shape/NHD_H_'+state+'_State_Shape.xml'
-        url2 = 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHD/State/HighResolution/Shape/NHD_H_'+state+'_State_Shape.zip'
-        wget.download(url1, 'D:\\NHD DataSet\\'+state+'.xml')
-        wget.download(url2, 'D:\\NHD DataSet\\'+state+'.zip')
-        
-    except Exception as e:
-        print(e)
-        print(state+' failed')
+
+
+
+
 
 """
-#TO DO 
-#READ THE FLOWLINES BY ID AND GET THE NAME
-#COMPARE THE NAME FOR SIMILARITY TO THE CLOSEST PUT IN GPS
-#IF COMPARISON IS TOO LOW GO BACK TO THE ORIGINAL
 
-sf = shapefile.Reader("D:\\NHD DataSet\\Shape\\NHDFlowline")
-shapes = sf.shapes()
-print(len(shapes))
-print(sf.fields)
-print(sf.record(0)[5])
-print
-for i in range(0,len(sf)):
-    print(sf.record(i)[5])
 
+def downloadStateReaches():
+#THIS FUNCTION DOWNLOADS THE NHD SHAPEFILES BY STATE (SHAPEFILES NOT AVAILABLE FOR WHOLE COUNTRY)
+    directory = os.getcwd()
+    #FIRST GET A LIST OF ALL THE STATES
+    for root, dirs, files in os.walk(directory): 
+        for file in files:
+            if file.startswith('stateNames'):
+                filePath = os.path.join(root, file)
+                states = pd.read_csv(filePath)
+    #THEN DOWNLOAD THE THE STATE SHAPEFILES AND XML
+    states = states['State'].values.tolist()
+    for state in states:
+        try:
+            url1 = 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHD/State/HighResolution/Shape/NHD_H_'+state+'_State_Shape.xml'
+            url2 = 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHD/State/HighResolution/Shape/NHD_H_'+state+'_State_Shape.zip'
+            wget.download(url1, 'D:\\NHD DataSet\\'+state+'.xml')
+            wget.download(url2, 'D:\\NHD DataSet\\'+state+'.zip')
+        except Exception as e:
+            print(e)
+            print(state+' failed')
+
+            #THE FLOW TABLE HAS THE COMID & GNIS_NAME EUREKA!!!
+#EXAMPLE BELOW FOR POUDRE RIVER
+temp = pd.read_csv('D:\\flowStateStorage\\flow_table.txt')
+print(temp.head())
+comid = temp['COMID'].values.tolist()
+name = temp['GNIS_NAME'].values.tolist()
+val = 2900003
+ind=comid.index(val)
+print(name[ind])
 
 #FIND SIMILARITY BETWEEN TWO STRINGS
 from difflib import SequenceMatcher
@@ -82,9 +89,7 @@ from difflib import SequenceMatcher
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
->>> similar("Apple","Appel")
-0.8
->>> similar("Apple","Mango")
-0.0
+similar("Apple","Appel")
+similar("Apple","Mango")
 
 """
